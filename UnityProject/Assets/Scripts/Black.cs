6e050521647fe9a4dedf5f6f46315da3066024f4
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-12-25 15:36:31 +0800

    完成敵人扣血死亡以及子彈生成,音效

--- UnityProject/Assets/Scripts/Black.cs
+++ UnityProject/Assets/Scripts/Black.cs
@@ -4,6 +4,36 @@ using UnityEngine.UI;
 public class Black : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+    [Header("血量")]
+    public int hp;
+    [Header("血量介面")]
+    public Text hpText;
+    [Header("音效區域")]
+    public AudioSource aud;
+    public AudioClip clip;
+
+    //碰撞時呼叫(因為子彈有on trigger所以用OnTriggerEnter)
+    private void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.gameObject.CompareTag("子彈"))
+        {
+            Hit();
+        }
+    }
+
+    private void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject,0.1f);
+        }
+    }
+
+    private void Hit()
+    {
+        hp -= 1;
+        hpText.text = hp.ToString();
+        aud.PlayOneShot(clip, 1);
+    }
     #endregion
 }
--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -1,9 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class White : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+    [Header("子彈")]
+    public GameObject bullet;
+    [Header("發射地點")]
+    public Transform point;
+    [Header("音效區域")]
+    public AudioSource aud;
+    public AudioClip clip;
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //生成子彈物件
+            Instantiate(bullet, point.position, Quaternion.identity);
+            aud.PlayOneShot(clip, 1);
+        }
+    }
     #endregion
 
     #region KID 區域 - 不要偷看 @3@
